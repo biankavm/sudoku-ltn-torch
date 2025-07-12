b7051e4cd3bd0254f9ca7e1a84b49fecac20d6ac
biankavm <bianka.maciel@jusbrasil.com.br>
2025-07-12 14:21:59 -0400

    feat: add read csv

--- utils/csv_handler.py
+++ utils/csv_handler.py
@@ -1,1 +1,10 @@
 # CSV file handling for Sudoku puzzles 
+
+import csv
+ 
+def read_csv(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        return list(reader)
+
+print(read_csv("../csvs/table1.csv"))
